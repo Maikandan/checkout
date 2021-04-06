665310ad543d1110799e062a33f559e2f1bc5a52
Maikandan Murugan <58242460+Maikandan@users.noreply.github.com>
2021-04-06 14:04:41 +0530

    Add files via upload

---
+++ checkout.js
@@ -0,0 +1,117 @@
+/* Set rates + misc */
+var taxRate = 0.05;
+var shippingRate = 15.00; 
+var fadeTime = 300;
+
+var orderQuantity;
+var orderLinePrice;
+
+/* Assign actions */
+$('.product-quantity input').change( function() {
+  updateQuantity(this);
+});
+
+$('.product-removal button').click( function() {
+  removeItem(this);
+});
+
+
+/* Recalculate cart */
+function recalculateCart()
+{
+  var subtotal = 0;
+  
+  /* Sum up row totals */
+  $('.product').each(function () {
+    subtotal += parseFloat($(this).children('.product-line-price').text());
+  });
+  
+  /* Calculate totals */
+  var tax = subtotal * taxRate;
+  var shipping = (subtotal > 0 ? shippingRate : 0);
+  var total = subtotal + tax + shipping;
+  
+  /* Update totals display */
+  $('.totals-value').fadeOut(fadeTime, function() {
+    $('#cart-subtotal').html(subtotal.toFixed(2));
+    $('#cart-tax').html(tax.toFixed(2));
+    $('#cart-shipping').html(shipping.toFixed(2));
+    $('#cart-total').html(total.toFixed(2));
+    if(total == 0){
+      $('.checkout').fadeOut(fadeTime);
+    }else{
+      $('.checkout').fadeIn(fadeTime);
+    }
+    $('.totals-value').fadeIn(fadeTime);
+  });
+}
+
+
+/* Update quantity */
+function updateQuantity(quantityInput)
+{
+  /* Calculate line price */
+  var productRow = $(quantityInput).parent().parent();
+  var price = parseFloat(productRow.children('.product-price').text());
+  var quantity = $(quantityInput).val();
+  var linePrice = price * quantity;
+
+  orderQuantity = quantity;
+  orderLinePrice = linePrice;
+  
+  /* Update line price display and recalc cart totals */
+  productRow.children('.product-line-price').each(function () {
+    $(this).fadeOut(fadeTime, function() {
+      $(this).text(linePrice.toFixed(2));
+      recalculateCart();
+      $(this).fadeIn(fadeTime);
+    });
+  });  
+}
+
+
+/* Remove item from cart */
+function removeItem(removeButton)
+{
+  /* Remove row from DOM and recalc cart total */
+  var productRow = $(removeButton).parent().parent();
+  productRow.slideUp(fadeTime, function() {
+    productRow.remove();
+    recalculateCart();
+  });
+}
+
+
+$(document).ready(function(){
+  //Menu Toggle Script
+  $("#menu-toggle").click(function(e) {
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
+  });
+  
+  // For highlighting activated tabs
+  $("#tab1").click(function () {
+  $(".tabs").removeClass("active1");
+  $(".tabs").addClass("bg-light");
+  $("#tab1").addClass("active1");
+  $("#tab1").removeClass("bg-light");
+  });
+  $("#tab2").click(function () {
+  $(".tabs").removeClass("active1");
+  $(".tabs").addClass("bg-light");
+  $("#tab2").addClass("active1");
+  $("#tab2").removeClass("bg-light");
+  });
+  $("#tab3").click(function () {
+  $(".tabs").removeClass("active1");
+  $(".tabs").addClass("bg-light");
+  $("#tab3").addClass("active1");
+  $("#tab3").removeClass("bg-light");
+  });
+  })
+
+  console.log(orderQuantity);
+  console.log(orderLinePrice);
+
+
+ 
